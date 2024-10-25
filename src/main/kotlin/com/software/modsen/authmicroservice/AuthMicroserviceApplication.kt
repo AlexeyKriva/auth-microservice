7e7d9dc7cf9d7f2583059e30e17d32b0875f46d2
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-10-25 21:10:46 +0300

    chore: init project

---
+++ src/main/kotlin/com/software/modsen/authmicroservice/AuthMicroserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.software.modsen.authmicroservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthMicroserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AuthMicroserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/software/modsen/authmicroservice/AuthMicroserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.software.modsen.authmicroservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthMicroserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
