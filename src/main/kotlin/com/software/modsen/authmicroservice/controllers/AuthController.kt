c5304f75cfbcfa509149a6ecb55b250160b5baf4
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-11-04 11:43:35 +0300

    feat: add auth controller

--- src/main/kotlin/com/software/modsen/authmicroservice/controllers/AuthController.kt
+++ src/main/kotlin/com/software/modsen/authmicroservice/controllers/AuthController.kt
@@ -1,17 +1,69 @@
 package com.software.modsen.authmicroservice.controllers
 
-import com.software.modsen.authmicroservice.entities.VerificationCode
-import com.software.modsen.authmicroservice.services.MailSenderService
+import com.software.modsen.authmicroservice.entities.auth.admin.AdminDetails
+import com.software.modsen.authmicroservice.entities.auth.driver.DriverDetails
+import com.software.modsen.authmicroservice.entities.auth.passenger.PassengerDetails
+import com.software.modsen.authmicroservice.entities.keycloak.KeycloakToken
+import com.software.modsen.authmicroservice.entities.keycloak.UserAuthDetails
+import com.software.modsen.authmicroservice.entities.mail.EmailConfirmation
+import com.software.modsen.authmicroservice.entities.mail.EmailForVerification
+import com.software.modsen.authmicroservice.entities.mail.VerificationCode
+import com.software.modsen.authmicroservice.services.AuthService
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.time.Duration
 
 @RestController
 @RequestMapping(value = ["/api/auth"], produces = ["application/json"])
-class AuthController(private val mailSenderService: MailSenderService) {
-    @PostMapping("/register")
-    public fun registration(): Unit {
+class AuthController(private val authService: AuthService) {
+    @PostMapping("/passengers/register")
+    fun registrationPassenger(
+        @Valid @RequestBody passengerDetails: PassengerDetails
+    ): ResponseEntity<KeycloakToken> {
+        return ResponseEntity(authService.savePassengerInKeycloak(passengerDetails), HttpStatus.CREATED)
+    }
+
+    @PostMapping("/drivers/register")
+    fun registrationDriver(
+        @Valid @RequestBody driverDetails: DriverDetails
+    ): ResponseEntity<KeycloakToken> {
+        return ResponseEntity(authService.saveDriverInKeycloak(driverDetails), HttpStatus.CREATED)
+    }
+
+    @PostMapping("/admins/register")
+    fun registrationAdmin(
+        @Valid @RequestBody adminDetails: AdminDetails
+    ): ResponseEntity<KeycloakToken> {
+        return ResponseEntity(authService.saveAdminInKeycloak(adminDetails), HttpStatus.CREATED)
+    }
+
+    @PostMapping("/users")
+    fun authUser(
+        @Valid @RequestBody userAuthDetails: UserAuthDetails
+    ): ResponseEntity<KeycloakToken> {
+        return ResponseEntity(authService.passAuthorization(userAuthDetails), HttpStatus.OK)
+    }
+
+    @PostMapping("/email/verify/resend")
+    fun resendVerificationCode(
+        @Valid @RequestBody emailForVerification: EmailForVerification
+    ): ResponseEntity<Unit> {
+        authService.resendVerificationCode(emailForVerification.email)
+
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+    @PostMapping("/email/verify")
+    fun verifyEmail(
+        @Valid @RequestBody emailConfirmation: EmailConfirmation
+    ): ResponseEntity<Unit> {
+        authService.verifyEmail(emailConfirmation.emailForVerification,
+            emailConfirmation.userVerificationCode)
+
+        return ResponseEntity(HttpStatus.OK)
     }
 }
