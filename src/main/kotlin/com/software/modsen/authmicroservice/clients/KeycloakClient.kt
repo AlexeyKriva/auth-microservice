40d2a56e92e32ec08ebe26583942cae819c9713b
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-11-04 11:34:12 +0300

    fix: update endpoints in keycloak client

--- src/main/kotlin/com/software/modsen/authmicroservice/clients/KeycloakClient.kt
+++ src/main/kotlin/com/software/modsen/authmicroservice/clients/KeycloakClient.kt
@@ -1,20 +1,13 @@
 package com.software.modsen.authmicroservice.clients
 
 import com.software.modsen.authmicroservice.config.FormFeignEncoderConfig
-import com.software.modsen.authmicroservice.entities.keycloak.KeycloakRole
-import com.software.modsen.authmicroservice.entities.keycloak.KeycloakToken
-import com.software.modsen.authmicroservice.entities.keycloak.KeycloakUser
+import com.software.modsen.authmicroservice.entities.keycloak.*
 import feign.Headers
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
 
 
 @FeignClient(name = "keycloak-server", configuration = [FormFeignEncoderConfig::class])
@@ -26,18 +19,22 @@ interface KeycloakClient {
         @RequestBody keycloakUser: KeycloakUser
     ): ResponseEntity<Unit>;
 
-    @PostMapping("/admin/realms/cab-agg/users/{id}/role-mappings/realm", consumes = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping("/admin/realms/{keycloakRealm}/users/{id}/role-mappings/realm", consumes =
+    [MediaType.APPLICATION_JSON_VALUE])
     fun giveRoleToUserInKeycloak(
+        @PathVariable("keycloakRealm") keycloakRealm: String,
         @RequestHeader("Authorization") authHeader: String,
         @PathVariable("id") userId: String,
         @RequestBody keycloakRoles: List<KeycloakRole>
     ): ResponseEntity<Unit>;
 
-    @GetMapping("/admin/realms/cab-agg/users")
+    @GetMapping("/admin/realms/{keycloakRealm}/users")
     fun getUsersByUsername(
+        @PathVariable("keycloakRealm") keycloakRealm: String,
         @RequestHeader("Authorization") authHeader: String,
-        @RequestParam("username") username: String
-    ): List<Map<String, Any>>;
+        @RequestParam(value = "username", required = false) username: String?,
+        @RequestParam(value = "email", required = false) email: String?
+    ): List<KeycloakUserDto>;
 
     @PostMapping("/realms/master/protocol/openid-connect/token",
         consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
@@ -51,4 +48,18 @@ interface KeycloakClient {
         @PathVariable("keycloakRealm") keycloakRealm: String,
         params: Map<String, Any>
     ): KeycloakToken
+
+    @GetMapping("/admin/realms/{keycloakRealm}/roles")
+    fun getUserRoles(
+        @PathVariable("keycloakRealm") keycloakRealm: String,
+        @RequestHeader("Authorization") authHeader: String
+    ): List<KeycloakRole>
+
+    @PutMapping("/admin/realms/{keycloakRealm}/users/{id}")
+    fun confirmEmail(
+        @PathVariable("keycloakRealm") keycloakRealm: String,
+        @RequestHeader("Authorization") authHeader: String,
+        @PathVariable("id") userId: String,
+        @RequestBody emailConfirmation: EmailConfirmation
+    ): ResponseEntity<Unit>
 }
