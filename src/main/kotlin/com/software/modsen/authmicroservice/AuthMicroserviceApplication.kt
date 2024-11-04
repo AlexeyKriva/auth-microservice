ec2336e718255368cf3bb8c7afcc6404361406f4
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-11-04 11:44:43 +0300

    chore: update security config, turn on feign clients and add dockerfile

--- src/main/kotlin/com/software/modsen/authmicroservice/AuthMicroserviceApplication.kt
+++ src/main/kotlin/com/software/modsen/authmicroservice/AuthMicroserviceApplication.kt
@@ -2,8 +2,12 @@ package com.software.modsen.authmicroservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableWebMvc
 class AuthMicroserviceApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/software/modsen/authmicroservice/config/SecurityConfig.kt
+++ src/main/kotlin/com/software/modsen/authmicroservice/config/SecurityConfig.kt
@@ -2,13 +2,9 @@ package com.software.modsen.authmicroservice.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
 import org.springframework.security.web.SecurityFilterChain
 import java.util.stream.Stream
 
@@ -18,30 +14,9 @@ class SecurityConfig {
     fun httpSecurity(httpSecurity: HttpSecurity): SecurityFilterChain {
         return httpSecurity
             .csrf {csrf -> csrf.disable()}
-            //.oauth2ResourceServer { oauth2 -> oauth2.jwt(Customizer.withDefaults()) }
             .authorizeHttpRequests { httpRequest -> httpRequest
                 .anyRequest().permitAll()
             }
             .build()
-    }
-
-    @Bean
-    fun jwtAuthenticationConverter(): JwtAuthenticationConverter {
-        val converter = JwtAuthenticationConverter()
-        val jwtGrantedAuthoritiesConverter = JwtGrantedAuthoritiesConverter()
-
-        converter.setPrincipalClaimName("preferred_username")
-        converter.setJwtGrantedAuthoritiesConverter { jwt ->
-            var authorities = jwtGrantedAuthoritiesConverter.convert(jwt) ?: emptyList()
-            var roles = jwt.getClaimAsStringList("cab_roles") ?: emptyList()
-
-            return@setJwtGrantedAuthoritiesConverter Stream.concat(authorities.stream(),
-                roles.stream()
-                    .filter { role -> role.startsWith("ROLE_") }
-                    .map { role -> SimpleGrantedAuthority(role) as GrantedAuthority })
-                .toList()
-        }
-
-        return converter
     }
 }
